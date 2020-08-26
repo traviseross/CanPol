b93259e951d4a93d0b184ff49d961a32839113e4
Travis Ross <travisross@terMBA.local>
2020-08-26 07:28:47 -0400

    loops through various search methods now

--- OCLC_Search_URL_Direct.py
+++ OCLC_Search_URL_Direct.py
@@ -13,13 +13,18 @@ def process_books(row):
     ''' (str,str) -> str
     take a list of authors and a publication title, output a worldcat search opensearch query with combined authors and title
     '''
+    failed = 0
+    worked = 0
+    query_base_URL = 'https://www.worldcat.org/search?q='
+    book_URL_constant = '&fq=+(x0:book-+OR+(x0:book+x4:printbook)+-((x0:book+x4:digital))+-((x0:book+x4:mic))+-((x0:book+x4:thsis))+-((x0:book+x4:mss))+-((x0:book+x4:braille))+-((x0:book+x4:continuing)))'
     pub_int = row['year']
     pub_date = str(pub_int)
-    pub_title = row['name']
+    title = row['name']
     # remove punctuation
-    pub_title = pub_title.translate(punctuation_dict)
-    #pub_title = 'srw.ti+=+"' + pub_title +'"'
-    #create a combined list of authors
+    pub_title = title.translate(punctuation_dict)
+    title_shortened = ' '.join(pub_title.split()[:5])
+    short_title = str(title_shortened)
+    #create a combined list of authors by surname
     authors = []
     for auth in ['a1','a2','a3','a4','a5','a6']:
         try:
@@ -28,27 +33,38 @@ def process_books(row):
             auth_surname = None
         if auth_surname != None:
            authors.append(auth_surname)
-    #author_list = "srw.au+=+" + "+and+srw.au+=+".join(authors)
-    author_list = " ".join(authors)
-    query_string = pub_title + "+and+" + author_list
-    # query_URL = "http://www.worldcat.org/webservices/catalog/search/worldcat/opensearch?q=" + query_string + "&recordSchema=info%3Asrw%2Fschema%2F1%2Fmarcxml&servicelevel=default&frbrGrouping=on&wskey={built-in-api-key}"
-    #query_URL = 'https://www.worldcat.org/search?q=ti:"' + pub_title + '"&dblist=638&fq=+(x0:book-+OR+(x0:book+x4:printbook)+-((x0:book+x4:digital))+-((x0:book+x4:mic))+-((x0:book+x4:thsis))+-((x0:book+x4:mss))+-((x0:book+x4:braille))+-((x0:book+x4:continuing)))+>+yr:' + pub_date
-    query_URL = 'https://www.worldcat.org/search?q=ti:"' + pub_title + '"&fq=+(x0:book-+OR+(x0:book+x4:printbook)+-((x0:book+x4:digital))+-((x0:book+x4:mic))+-((x0:book+x4:thsis))+-((x0:book+x4:mss))+-((x0:book+x4:braille))+-((x0:book+x4:continuing)))+yr:' + pub_date
+    author_list = "+au:".join(authors)
+    title_date_query_URL = query_base_URL + 'ti:' + pub_title + book_URL_constant + '+yr:' + pub_date
+    author_title_query_URL =  query_base_URL + 'ti:' + pub_title + author_list + book_URL_constant
+    short_title_date_query_URL = query_base_URL + 'ti:' + short_title + book_URL_constant + '+yr:' + pub_date
+    short_title_author_query = query_base_URL + 'ti:' + short_title + author_list + book_URL_constant
     '''
     print(query_URL)
     '''
+    query_list = [title_date_query_URL, author_title_query_URL, short_title_date_query_URL, short_title_author_query]
     OCLC_list = []
-    results_page =  requests.get(query_URL)
-    page_soup = BeautifulSoup(results_page.content, 'html.parser')
-    table_soup = page_soup.find("table",{"class": "table-results"})
-    if table_soup:
-        results_table = table_soup.find_all("div", class_="oclc_number")
-        for row in results_table:
-            OCLC_list.append(row.text)
+    i = 0
+    while len(OCLC_list) < 1 and i <= 3:
+        query_URL = query_list[i]
+        results_page =  requests.get(query_URL)
+        page_soup = BeautifulSoup(results_page.content, 'html.parser')
+        table_soup = page_soup.find("table",{"class": "table-results"})
+        if table_soup:
+            results_table = table_soup.find_all("div", class_="oclc_number")
+            for row in results_table:
+                OCLC_list.append(row.text)
+        else:
+            try_number = str(i)
+        i = i + 1
+    if len(OCLC_list) > 0:
+        number_of_attempts =  str(i)
+        number_of_OCLCs = str(len(OCLC_list))
+        worked += worked
+        print("Another success, moving on.")
     else:
-        print("these are not the books you're looking for, move along")
-    print(pub_title, OCLC_list)
-
+        failed += failed
+        "Failed, but moving on."
+    
     
     
     ######################
